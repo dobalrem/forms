48431a39c79040143597e0f9889762c7aca4c713
dobalrem <166685837+dobalrem@users.noreply.github.com>
2024-04-12 08:31:26 +0800

    Delete js/new.js

--- js/new.js
+++
@@ -1,9 +0,0 @@
-function updatePrintCount() {
-    var printCountInput = document.getElementById('printCountInput');
-    var inputValue = printCountInput.value.trim();
-
-    //Extract the numerical part form the input value
-    var numericPart = inputValue.match(/\d+/);
-    var printCountNumeric = parseInt(numericPart[0], 10);
-    var numericPart = inputValue
-}
